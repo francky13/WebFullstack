feecdf28e05398dbabf5f8777a7c36ed2ee01c44
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-28 17:05:25 +0300

    whith template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component : WelcomeComponent},
+
+
+{
+    path: '',
+    redirectTo: 'Liste',
+    pathMatch: 'full'
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
